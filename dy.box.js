c16beadf34e6c5b5a65ced1295ea79222cd3481e
gz154838009 <35130785+gz154838009@users.noreply.github.com>
2021-06-27 23:42:14 +0800

    Create dy.box.js

---
+++ dy.box.js
@@ -0,0 +1,14 @@
+    {
+      "id": "dyjs",
+      "name": "抖音极速版刷音符",
+      "keys": ["dyheadlist"],
+      "descs_html": [],
+      "settings": [],
+      "icons": [
+        "https://gitee.com/passerby-b/javascript/raw/master/icon/dyjsb.png",
+        "https://gitee.com/passerby-b/javascript/raw/master/icon/dyjsb.png"
+      ],
+      "author": "@passerby-b",
+      "repo": "https://gitee.com/passerby-b/javascript/raw/master/JD/passerby-b.boxjs.json",
+      "script": "https://gitee.com/passerby-b/javascript/raw/master/dyjs.js"
+    }
